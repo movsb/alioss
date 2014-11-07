fa2170f2457aac59aa8924ff7f99da6b29718c78
twofei <anhbk@qq.com>
2014-11-08 03:46:15 +0800

    add module: basic byte stream interface

---
+++ src/misc/stream.h
@@ -0,0 +1,37 @@
+/*---------------------------------------------------------
+File	: stream
+Author	: twofei <anhbk@qq.com>
+Date	: Sat, Nov 8 2014
+Desc	: I'm not familiar with writing a class template,
+	So it's still a class with pure virtual functions
+-----------------------------------------------------------*/
+
+#pragma once
+
+#ifndef __alioss_misc_stream_h__
+#define __alioss_misc_stream_h__
+
+namespace alioss{
+
+namespace stream{
+
+	class stream_size {
+	public:
+		virtual int size() const = 0;
+	};
+
+	class istream : public stream_size {
+	public:
+		virtual int read_some(const void* buf, int sz) = 0;
+	};
+
+	class ostream : public stream_size {
+	public:
+		virtual int write_some(const void* buf, int sz) = 0;
+	};
+
+} // namespace stream
+
+} // namespace alioss
+
+#endif // !__alioss_misc_stream_h__
