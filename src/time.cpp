1e4658d27e4cc390f86b2c7305a77e0b57e746b4
twofei <anhbk@qq.com>
2014-11-08 15:06:01 +0800

    time: a little change: reference & namespace

--- src/time.cpp
+++ src/time.cpp
@@ -1,3 +1,4 @@
+#include <ctime>
 #include "time.h"
 
 namespace alioss {
@@ -5,13 +6,13 @@ namespace alioss {
 std::string gmt_time(){
 	time_t rawtime;
 	tm* ptm;
-	time(&rawtime);
-	ptm = gmtime(&rawtime);
+	::time(&rawtime);
+	ptm = ::gmtime(&rawtime);
 
 	char str[32];
-	strftime(str, 32, "%a, %d %b %Y %H:%M:%S GMT", ptm);
+	// http://en.cppreference.com/w/c/chrono/strftime
+	::strftime(str, 32, "%a, %d %b %Y %H:%M:%S GMT", ptm);
 	return str;
 }
 
-}
-
+} // namespace alioss
--- src/time.h
+++ src/time.h
@@ -1,5 +1,4 @@
 #include <string>
-#include <ctime>
 
 namespace alioss{
 
