f0ec67a1b683a08333972c4d5cd441a40ee10736
twofei <anhbk@qq.com>
2014-11-08 15:07:30 +0800

    Service(Buckets) module: added

---
+++ src/service.cpp
@@ -0,0 +1,104 @@
+#include <stdexcept>
+#include <iostream>
+
+#include "time.h"
+#include "signature.h"
+#include "socket.h"
+#include "misc/stream.h"
+#include "service.h"
+
+namespace alioss{
+
+namespace service{
+
+bool service::connect()
+{
+	socket::resolver resolver;
+	resolver.resolve("oss.aliyuncs.com", "http");
+	return _http.connect(resolver[0].c_str(), 80);
+}
+
+bool service::disconnect()
+{
+	return _http.disconnect();
+}
+
+bool service::query()
+{
+	request();
+
+	_http.get_head();
+
+	class body_stream : public stream::ostream {
+	public:
+		body_stream()
+			: _size(0)
+		{}
+
+	public:
+		virtual int size() const override {
+			return _size;
+		}
+
+		virtual int write_some(const unsigned char* buf, int sz) override
+		{
+			std::string str((char*)buf, sz);
+			std::cout << str;
+
+			_size += sz;
+
+			return sz;
+		}
+
+	protected:
+		int _size;
+	};
+
+	body_stream bs;
+
+	_http.get_body(bs);
+
+	return true;
+}
+
+bool service::request()
+{
+	auto& head = _http.head();
+	head.clear();
+
+	std::string str;
+	std::stringstream ss;
+
+	// Verb
+	ss.clear();
+	ss << "GET / HTTP/1.1";
+	str = ss.str();
+	head.set_verb(str.c_str());
+
+	// Host
+	head.add_host("oss.aliyuncs.com");
+
+	// Date
+	std::string date(gmt_time());
+	head.add_date(date.c_str());
+
+	// Authorization
+	std::string sigstr;
+	sigstr += "GET\n";		// Verb
+	sigstr += "\n\n";		// Content-MD5 & Content-Type
+	sigstr += date + "\n";	// GMT Date/Time
+	sigstr += "/";			// Resource
+
+	std::string authstr(signature(_key, sigstr));
+	head.add_authorization(authstr.c_str());
+
+	// Connection
+	head.add_connection("close");
+
+	return _http.put_head();
+}
+
+} // namespace service
+
+} // namespace alioss
+
---
+++ src/service.h
@@ -0,0 +1,125 @@
+/*-----------------------------------------------
+File	: service.h
+Date	: Nov 6, 2014
+Author	: twofei <anhbk@qq.com>
+-----------------------------------------------*/
+
+#include <stdint.h>
+
+#include <string>
+#include <sstream>
+#include <vector>
+
+#include "socket.h"
+#include "accesskey.h"
+
+namespace alioss {
+
+namespace service{
+
+class bucket{
+public:
+	bucket() {}
+
+	bucket(const char* name,const char*	loc, const char* date)
+		: _name(name)
+		, _location(loc)
+		, _creation_date(date)
+	{}
+
+	void set_name(const char* name) {
+		_name = name;
+	}
+	const std::string& name() const {
+		return _name;
+	}
+
+	void set_location(const char* loc){
+		_location = loc;
+	}
+	const std::string& location() const {
+		return _location;
+	}
+	
+	void set_creation_date(const char* date){
+		_creation_date = date;
+	}
+	const std::string& creation_date() const {
+		return _creation_date;
+	}
+
+protected:
+	std::string _name;
+	std::string _location;
+	std::string _creation_date;
+};
+
+class owner {
+public:
+	owner() {}
+	owner(const char* id, const char* name)
+		: _id(id)
+		, _display_name(name)
+	{}
+
+	void set_id(const char* id) {
+		_id = id;
+	}
+
+	void set_display_name(const char* name){
+		_display_name = name;
+	}
+
+	const std::string& id() const {
+		return _id;
+	}
+
+	const std::string& display_name() const {
+		return _display_name;
+	}
+
+protected:
+	std::string _id;
+	std::string _display_name;
+};
+
+class service {
+public:
+	service()
+	{}
+	
+	bool connect();
+	bool disconnect();
+
+	void set_key(const char* id, const char* secret){
+		_key.set_key(id, secret);
+	}
+
+	bool query();
+
+protected:
+	bool request();
+	bool response();
+
+	bool parse_response_body();
+
+protected:
+	std::string		_prefix;
+	std::string		_marker;
+	std::string		_max_keys;
+	bool			_is_truncated;
+	std::string		_next_marker;
+
+protected:
+	owner _owner;
+	std::vector<bucket> _buckets;
+
+protected:
+	accesskey	_key;
+	http::http	_http;
+};
+
+} // namespace service
+
+} // namespace alioss
+
