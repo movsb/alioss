e80e6dc06f8e8945192c788820a9a02508eee58d
twofei <anhbk@qq.com>
2014-11-09 15:37:30 +0800

    osserror: format with color-terminal module

--- src/osserror.cpp
+++ src/osserror.cpp
@@ -2,6 +2,7 @@
 
 #include <tinyxml2/tinyxml2.h>
 
+#include "misc/color_term.h"
 #include "osserror.h"
 
 namespace alioss {
@@ -23,18 +24,22 @@ void osserr::parse(const char* xml, int size)
 }
 
 void ossexcept_stderr_dumper(ossexcept& e){
-	std::cerr << "-->Dumping osserr:\n";
+	color_term::color_term cterm;
+
+	std::cerr << cterm(12,-1) << "-->Dumping osserr:\n" << cterm.restore();
 	e.dump_osserr([](const std::string& s){
 		std::cerr << "    " << s << std::endl;
 	});
 
-	std::cerr << "-->ExceptionCode: \n    " << (int)e.code() << std::endl;
-	std::cerr << "-->ExceptionMsg : \n    " << e.what() << std::endl;
+	std::cerr << cterm(12, -1) << "-->ExceptionCode: \n    " << cterm.restore() << (int)e.code() << std::endl;
+	std::cerr << cterm(12, -1) << "-->ExceptionMsg : \n    " << cterm.restore() << e.what() << std::endl;
 
-	std::cerr << "-->Dumping exception stack:\n";
-	e.dump_stack([](int i, const std::string& stk){
-		std::cerr << "    " << i << ": " << stk << std::endl;
+	std::cerr << cterm(12, -1) << "-->Dumping exception stack:\n" << cterm.restore();
+	e.dump_stack([&](int i, const std::string& stk){
+		std::cerr << "    " << cterm(2,-1) << i << cterm(-1,-1) << ": " << stk << std::endl;
 	});
+
+	std::cerr << cterm.restore();
 }
 
 
