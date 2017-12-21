4e519774c9014dd19f9f9e6417a05b246967563f
movsb <anhbk@qq.com>
2017-12-21 17:39:03 +0800

    let's Go!

---
+++ sign.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base64"
+	"strconv"
+)
+
+type xAccessKey struct {
+	key    []byte
+	secret []byte
+}
+
+func sign(key *xAccessKey, msg string) string {
+	h := hmac.New(sha1.New, key.secret)
+	h.Write([]byte(msg))
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
+}
+
+func signHead(key *xAccessKey, verb string, date string, resource string) string {
+	msg := verb + "\n" + "\n" + "\n" + date + "\n" + resource
+	mac := sign(key, msg)
+	return "OSS " + key.key + ":" + mac
+}
+
+func signURL(key *xAccessKey, expiration uint, resource string) string {
+	msg := "GET\n\n\n" + strconv.Itoa(expiration) + "\n" + resource
+	return sign(key, msg)
+}
